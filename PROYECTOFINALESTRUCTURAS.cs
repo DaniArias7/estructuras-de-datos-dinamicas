c27effd48b7bf1914458852fc51b8669dde215f0
DaniArias7 <129128034+DaniArias7@users.noreply.github.com>
2023-11-16 14:14:45 -0500

    PROYECTOFINALESTRUCTURAS.cs
    FINAL

--- PROYECTOFINALESTRUCTURAS.cs
+++ PROYECTOFINALESTRUCTURAS.cs
@@ -1,5 +1,5 @@
-//PROYECTO FINAL
-// DANIEL ALZATE
+// PROYECTO FINAL ESTRUCTURAS DE DATOS 
+// DANIEL ALZATE ARIAS
 
 using System;
 using System.Collections.Generic;
@@ -13,41 +13,112 @@ public class BinaryTree
 
     public BinaryTree(int value, BinaryTree parent = null, BinaryTree left = null, BinaryTree right = null)
     {
+        if (parent == null && (left != null || right != null))
+        {
+            throw new ArgumentException("El nodo padre no puede ser nulo.");
+        }
+
+        if (ValueAlreadyExists(parent, value))
+        {
+            throw new ArgumentException($"El valor {value} ya existe en el árbol.");
+        }
+
         Value = value;
         Parent = parent;
         Left = left;
         Right = right;
     }
+
+    private static bool ValueAlreadyExists(BinaryTree node, int value)
+    {
+        if (node == null)
+        {
+            return false;
+        }
+
+        if (node.Value == value)
+        {
+            return true;
+        }
+
+        return ValueAlreadyExists(node.Left, value) || ValueAlreadyExists(node.Right, value);
+    }
+
+    public void PrintTree()
+    {
+        PrintTree(this, "");
+    }
+
+    private void PrintTree(BinaryTree node, string indent, bool isLast = true)
+    {
+        if (node != null)
+        {
+            Console.Write(indent);
+
+            if (isLast)
+            {
+                Console.Write("└─ ");
+                indent += "   ";
+            }
+            else
+            {
+                Console.Write("├─ ");
+                indent += "│  ";
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{node.Value}");
+            Console.ResetColor();
+
+            if (node.Left != null || node.Right != null)
+            {
+                PrintTree(node.Left, indent, node.Right == null);
+                PrintTree(node.Right, indent, true);
+            }
+        }
+    }
 }
 
-public class TreeOrder
+public class TreeTraversal
 {
-    public static void InOrder(BinaryTree root, Action<BinaryTree> callback)
+    public static void InOrderTraversal(BinaryTree root, Action<BinaryTree> callback)
     {
         if (root == null)
         {
-            throw new ArgumentNullException(nameof(root), "El árbol no puede ser nulo.");
+            throw new ArgumentNullException(nameof(root), "El árbol no puede ser nulo");
         }
 
         if (callback == null)
         {
-            throw new ArgumentNullException(nameof(callback), "La función de devolución de llamada no puede ser nula.");
+            throw new ArgumentNullException(nameof(callback), "La función de devolución de llamada no puede ser nula");
         }
 
         Stack<BinaryTree> stack = new Stack<BinaryTree>();
         BinaryTree currentNode = root;
 
-        while (currentNode != null || stack.Count > 0)
+        try
         {
-            while (currentNode != null)
+            while (currentNode != null || stack.Count > 0)
             {
-                stack.Push(currentNode);
-                currentNode = currentNode.Left;
-            }
+                while (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
 
-            currentNode = stack.Pop();
-            callback(currentNode);
-            currentNode = currentNode.Right;
+                currentNode = stack.Pop();
+
+                Console.ForegroundColor = ConsoleColor.Blue; // Set color to blue for callback results
+                callback(currentNode);
+                Console.ResetColor(); // Reset color to default
+
+                currentNode = currentNode.Right;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error durante el recorrido del árbol: {ex.Message}");
+            Console.WriteLine($"StackTrace: {ex.StackTrace}");
         }
     }
 }
@@ -56,26 +127,107 @@ class Program
 {
     static void Main()
     {
-        // Ejemplo de uso
-        BinaryTree root = new BinaryTree(1);
-        root.Left = new BinaryTree(2, root);
-        root.Right = new BinaryTree(3, root);
-        root.Left.Left = new BinaryTree(4, root.Left);
-        root.Left.Left.Right = new BinaryTree(9, root.Left.Left);
-        root.Right.Left = new BinaryTree(6, root.Right);
-        root.Right.Right = new BinaryTree(7, root.Right);
-
-        // Definir la función de devolución de llamada
-        Action<BinaryTree> callback = (node) => Console.WriteLine($"callback({node.Value})");
-
-        // Realizar el recorrido in-order 
-        try
+        do
         {
-            TreeOrder.InOrder(root, callback);
-        }
-        catch (Exception ex)
+            Console.WriteLine("Construyendo un Árbol Binario");
+
+            int valorRaiz = ObtenerEntradaUsuario("Ingresa el valor para el nodo raíz:");
+
+            BinaryTree raiz = new BinaryTree(valorRaiz);
+
+            try
+            {
+                ConstruirArbol(raiz);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al construir el árbol: {ex.Message}");
+                return;
+            }
+
+            Console.WriteLine("Recorrido en Orden:");
+            TreeTraversal.InOrderTraversal(raiz, nodo =>
+            {
+                Console.ForegroundColor = ConsoleColor.Blue; // Set color to blue for callback results
+                Console.WriteLine($"Callback({nodo.Value})");
+                Console.ResetColor(); // Reset color to default
+            });
+
+            Console.WriteLine("\nÁrbol Binario:");
+            raiz.PrintTree();
+
+        } while (PreguntarSiOtroArbol("¿Quieres crear otro árbol?"));
+
+        Console.WriteLine("¡Hasta luego!");
+    }
+
+
+    static void ConstruirArbol(BinaryTree nodoPadre)
+    {
+        Console.WriteLine($"Nodo Actual ({(nodoPadre.Parent != null ? "Padre= " + nodoPadre.Parent.Value : "Raíz")}) : {nodoPadre.Value}");
+
+        if (PreguntarSiONo("¿Quieres agregar un hijo izquierdo?"))
         {
-            Console.WriteLine($"Se produjo una excepción: {ex.Message}");
+            int valorIzquierdo = ObtenerEntradaUsuario($"Ingresa el valor para el hijo izquierdo del nodo {nodoPadre.Value}:");
+
+            try
+            {
+                nodoPadre.Left = new BinaryTree(valorIzquierdo, nodoPadre);
+                ConstruirArbol(nodoPadre.Left);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error al agregar el hijo izquierdo: {ex.Message}");
+            }
         }
+
+        Console.WriteLine($"Callback({nodoPadre.Value})");
+
+        if (PreguntarSiONo("¿Quieres agregar un hijo derecho?"))
+        {
+            int valorDerecho = ObtenerEntradaUsuario($"Ingresa el valor para el hijo derecho del nodo {nodoPadre.Value}:");
+
+            try
+            {
+                nodoPadre.Right = new BinaryTree(valorDerecho, nodoPadre);
+                ConstruirArbol(nodoPadre.Right);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error al agregar el hijo derecho: {ex.Message}");
+            }
+        }
+    }
+
+
+    static int ObtenerEntradaUsuario(string mensaje)
+    {
+        Console.WriteLine(mensaje);
+        int entradaUsuario;
+
+        while (!int.TryParse(Console.ReadLine(), out entradaUsuario))
+        {
+            Console.WriteLine("Entrada no válida. Por favor, ingresa un entero válido.");
+        }
+
+        return entradaUsuario;
+    }
+
+    static bool PreguntarSiONo(string mensaje)
+    {
+        Console.WriteLine($"{mensaje} (responde 's' para sí o 'n' para no):");
+
+        string entradaUsuario = Console.ReadLine().ToLower();
+
+        return entradaUsuario == "s" || entradaUsuario == "si";
+    }
+
+    static bool PreguntarSiOtroArbol(string mensaje)
+    {
+        Console.WriteLine($"{mensaje} (responde 's' para sí o 'n' para no):");
+
+        string entradaUsuario = Console.ReadLine().ToLower();
+
+        return entradaUsuario == "s" || entradaUsuario == "si";
     }
 }
